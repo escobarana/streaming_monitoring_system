5677eba933dcc65df6911fd829bbea586b1818dd
Ana Escobar Llamazares <36671219+escobarana@users.noreply.github.com>
2022-11-04 11:28:39 +0100

    Revert "Update: Prediction of failure in real time and flask app with HTML webpage"

--- model/model_training.py
+++ model/model_training.py
@@ -1,22 +1,8 @@
-from sklearn.model_selection import train_test_split
-import config
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+class Model:
 
-
-
-class MODEL:
-
-    def __init__(self,columns,data):
+    def __init__(self, columns, data):
         self.columns = columns
         self.data = data
 
-    
     def develop_model(self):
         pass
-        
