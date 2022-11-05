fdb09adf5fec526e3b6b1f5aed1b0714a8da1108
Ana Escobar <anaescobar8@gmail.com>
2022-11-05 21:58:44 +0100

    feat(iac): automate deployment in AWS using terraform

---
+++ iac/README.md
@@ -0,0 +1,12 @@
+### https://aws.amazon.com/es/blogs/opensource/deploying-python-flask-microservices-to-aws-using-open-source-tools/
+# Useful comands:
+```shell
+    cd iac -- move to iac directory
+    terraform login -- only if necessary (to log in in terraform cloud)
+    terraform init -- initializes terraform in the current folder
+    terraform fmt -- formats terraform files
+    terraform validate -- validates terraform files (syntax)
+    terraform plan -- shows plan (what is new from the current .tfstate file)
+    terraform apply -- creates/applies plan
+    terraform destroy -- destroy all reaources
+```
---
+++ iac/config.tf
@@ -0,0 +1,34 @@
+# This configuration file will contain the provider configurations and tested versions.
+# Specify the Terraform provider version.
+# Configure the AWS Provider
+
+# Configure the Docker & AWS Providers
+terraform {
+  required_providers {
+    docker = {
+      source    = "kreuzwerker/docker"
+      version   = "~> 2.20.0"
+    }
+    aws = {
+      source    = "hashicorp/aws"
+      version   = "~> 4.16"
+    }
+  }
+
+  cloud {
+    organization = "escobarana"
+
+    workspaces {
+      name = "gh-actions-kafkaproject"
+    }
+  }
+}
+
+provider "docker" {}  # Pull the image from Docker Hub
+
+provider "aws" {  # Configure AWS
+  region = var.region
+  # --- COMMENT THESE TWO VARIABLES BELOW WHEN AUTOMATIZING THE DEPLOYMENT WITH GITHUB ACTIONS --- #
+//  access_key = var.access_key
+//  secret_key = var.secret_access_key
+}
