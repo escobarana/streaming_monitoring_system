b8f272d70c0120cf3fbe570ddcc646c594b261af
Ana Escobar <anaescobar8@gmail.com>
2022-11-28 12:39:19 +0100

    fix: aws region as env var

--- model/data_retriever.py
+++ model/data_retriever.py
@@ -5,7 +5,7 @@ import os
 
 dynamodb = boto3.resource(
     'dynamodb',
-    region_name="us-east-1",
+    region_name=os.environ["AWS_REGION_NAME"],
     aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
 )
